0accf5252af8bcb4929d0f316ff5e3b6db99c950
Daniel Hoksza <daniel.hoksza@gmail.com>
2016-02-24 13:32:27 +0000

    making gulpfile more modular

---
+++ gulp/test.js
@@ -0,0 +1,49 @@
+
+
+$('')
+.append()
+.append()
+.append()
+//here
+.append();
+
+$('')
+.append()
+.append()
+.append()
+//here
+.append();
+
+function sampleGulp(){
+	this.pipe = function(){
+		console.log('Something');
+		return this;
+	};
+	
+	this.pipeEndProcess = function(){
+		console.log('pipeEndProcess');
+		return this;
+	};
+}
+
+new sampleGulp()
+.pipe()
+.pipe()
+.pipe()
+.pipe()
+.pipeEndProcess();
+
+
+
+var obj = {
+	name: 'Daniel',
+	info: {
+		email: 'example@example.com'
+	}
+};
+
+console.log(obj.info.email);
+
+var key = 'info',
+  key2 = 'email';
+console.log(obj[key][key2]);
