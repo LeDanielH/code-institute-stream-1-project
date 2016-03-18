ce459f7a93e069f0fdc9a33c2a927296642fd2d9
Daniel Hoksza <daniel.hoksza@gmail.com>
2016-03-18 00:56:14 +0000

    fixing small errors

--- app/scripts/my-scripts.js
+++ app/scripts/my-scripts.js
@@ -381,22 +381,23 @@ angular.module('bandApp', [
 	angular.module('myBandAppServices')
 		.factory('CallToActionDataService', [
         '$resource',
-        function ($resource) {
-				var calls = {
-					go: function (path) {
-						$location.path(path);
-					},
-					callToActionBoxes: $resource('data/json/:itemId.json', {}, {
-						query: {
-							method: 'GET',
-							params: {
-								itemId: 'calls-to-action'
-							},
-							isArray: false
-						}
-					})
-				};
-				return calls;
+        '$location',
+        function ($resource, $location) {
+			var calls = {
+				go: function (path) {
+					$location.path(path);
+				},
+				callToActionBoxes: $resource('data/json/:itemId.json', {}, {
+					query: {
+						method: 'GET',
+						params: {
+							itemId: 'calls-to-action'
+						},
+						isArray: false
+					}
+				})
+			};
+			return calls;
         }
     ]);
 }());
