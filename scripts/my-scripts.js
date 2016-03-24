be9b60869eb7ea43589c90712591fe7f83037b50
Daniel Hoksza <daniel.hoksza@gmail.com>
2016-03-24 00:41:35 +0000

    Update 2016-03-24T00:41:29.948Z

--- scripts/my-scripts.js
+++ scripts/my-scripts.js
@@ -882,39 +882,39 @@ angular.module('bandApp', [
 			};
 		});
 }());
-;(function () {
-	'use strict';
-	angular.module('myBandAppAnimations')
-		.animation('.testimonials-animation', function() {
-			return {
-				addClass: function(element, className, done) {
-					var scope = element.scope();
-					var parentWidth = angular.element(document.querySelectorAll(".slide-viewer"))[0].getBoundingClientRect().width;
-					if (className === 'ng-hide') {
-						var finishPoint = parentWidth;
-						if (scope.direction !=='right') {
-							finishPoint = -finishPoint;
-						}
-						TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
-					} else {
-						done();
-					}
-				},
-				removeClass: function(element, className, done) {
-					var scope = element.scope();
-					var parentWidth = angular.element(document.querySelectorAll(".slide-viewer"))[0].getBoundingClientRect().width;
-					if (className === 'ng-hide') {
-						element.removeClass('ng-hide');
-						var startPoint = parentWidth;
-						if (scope.direction === 'right') {
-							startPoint = -startPoint;
-						}
-						TweenMax.set(element, {left: startPoint});
-						TweenMax.to(element, 0.5, {left: 0, onComplete: done});
-					} else {
-						done();
-					}
-				}
-			};
-		});
-}());
+// ;(function () {
+// 	'use strict';
+// 	angular.module('myBandAppAnimations')
+// 		.animation('.testimonials-animation', function() {
+// 			return {
+// 				addClass: function(element, className, done) {
+// 					var scope = element.scope();
+// 					var parentWidth = angular.element(document.querySelectorAll(".slide-viewer"))[0].getBoundingClientRect().width;
+// 					if (className === 'ng-hide') {
+// 						var finishPoint = parentWidth;
+// 						if (scope.direction !=='right') {
+// 							finishPoint = -finishPoint;
+// 						}
+// 						TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
+// 					} else {
+// 						done();
+// 					}
+// 				},
+// 				removeClass: function(element, className, done) {
+// 					var scope = element.scope();
+// 					var parentWidth = angular.element(document.querySelectorAll(".slide-viewer"))[0].getBoundingClientRect().width;
+// 					if (className === 'ng-hide') {
+// 						element.removeClass('ng-hide');
+// 						var startPoint = parentWidth;
+// 						if (scope.direction === 'right') {
+// 							startPoint = -startPoint;
+// 						}
+// 						TweenMax.set(element, {left: startPoint});
+// 						TweenMax.to(element, 0.5, {left: 0, onComplete: done});
+// 					} else {
+// 						done();
+// 					}
+// 				}
+// 			};
+// 		});
+// }());
