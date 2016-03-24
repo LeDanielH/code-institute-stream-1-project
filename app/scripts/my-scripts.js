309ede2693a53322879a36f88e9d19e88d442e96
Daniel Hoksza <daniel.hoksza@gmail.com>
2016-03-24 00:46:12 +0000

    fixed photo slider

--- app/scripts/my-scripts.js
+++ app/scripts/my-scripts.js
@@ -846,42 +846,42 @@ angular.module('bandApp', [
 	'use strict';
 	angular.module('myBandAppAnimations', []);
 }());
-;(function () {
-	'use strict';
-	angular.module('myBandAppAnimations')
-		.animation('.store-items-slider-animation', function() {
-			return {
-				addClass: function(element, className, done) {
-					var scope = element.scope();
-					var itemWidth = angular.element(document.querySelectorAll(".slider-viewer"))[0].getBoundingClientRect().width;
-					if (className === 'ng-hide') {
-						var finishPoint = itemWidth;
-						if (scope.direction !=='right') {
-							finishPoint = -finishPoint;
-						}
-						TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
-					} else {
-						done();
-					}
-				},
-				removeClass: function(element, className, done) {
-					var scope = element.scope();
-					var itemWidth = angular.element(document.querySelectorAll(".slider-viewer"))[0].getBoundingClientRect().width;
-					if (className === 'ng-hide') {
-						element.removeClass('ng-hide');
-						var startPoint = itemWidth;
-						if (scope.direction === 'right') {
-							startPoint = -startPoint;
-						}
-						TweenMax.set(element, {left: startPoint});
-						TweenMax.to(element, 0.5, {left: 0, onComplete: done});
-					} else {
-						done();
-					}
-				}
-			};
-		});
-}());
+// ;(function () {
+// 	'use strict';
+// 	angular.module('myBandAppAnimations')
+// 		.animation('.store-items-slider-animation', function() {
+// 			return {
+// 				addClass: function(element, className, done) {
+// 					var scope = element.scope();
+// 					var itemWidth = angular.element(document.querySelectorAll(".slider-viewer"))[0].getBoundingClientRect().width;
+// 					if (className === 'ng-hide') {
+// 						var finishPoint = itemWidth;
+// 						if (scope.direction !=='right') {
+// 							finishPoint = -finishPoint;
+// 						}
+// 						TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
+// 					} else {
+// 						done();
+// 					}
+// 				},
+// 				removeClass: function(element, className, done) {
+// 					var scope = element.scope();
+// 					var itemWidth = angular.element(document.querySelectorAll(".slider-viewer"))[0].getBoundingClientRect().width;
+// 					if (className === 'ng-hide') {
+// 						element.removeClass('ng-hide');
+// 						var startPoint = itemWidth;
+// 						if (scope.direction === 'right') {
+// 							startPoint = -startPoint;
+// 						}
+// 						TweenMax.set(element, {left: startPoint});
+// 						TweenMax.to(element, 0.5, {left: 0, onComplete: done});
+// 					} else {
+// 						done();
+// 					}
+// 				}
+// 			};
+// 		});
+// }());
 ;(function () {
 	'use strict';
 	angular.module('myBandAppAnimations')
